e26b9305aa67904aa5e921e74f221aefc2cad034
Christian Gamperl <christian@gamperl.me>
2025-02-15 09:24:11 +0100

    Mark listener as FunctionalInterface

--- app/src/main/java/at/wifi/swdev/noteapp/listener/OnListItemClickListener.java
+++ app/src/main/java/at/wifi/swdev/noteapp/listener/OnListItemClickListener.java
@@ -2,6 +2,7 @@ package at.wifi.swdev.noteapp.listener;
 
 import at.wifi.swdev.noteapp.database.entity.Note;
 
+@FunctionalInterface
 public interface OnListItemClickListener {
     void onListItemClick(Note note, int position);
 }
